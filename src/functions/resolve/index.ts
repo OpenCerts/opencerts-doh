0d3440d1b7ab2d7a452f5c6f03a2a0fbe459e1b8
Seth <hullogh@gmail.com>
2024-10-09 14:49:36 +0800

    feat: add cors origins

--- src/functions/resolve/index.ts
+++ src/functions/resolve/index.ts
@@ -1,21 +1,16 @@
-import { handlerPath } from '@libs/handler-resolver';
+import { handlerPath } from "@libs/handler-resolver";
 import { AWS } from "@serverless/typescript";
 
-const config: AWS['functions']['Function'] = {
+const config: AWS["functions"]["Function"] = {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
     {
       http: {
-        method: 'get',
-        path: 'resolve',
-        cors: {
-          origin: 'https://www.opencerts.io',
-          headers: ['Accept'],
-          allowCredentials: false,
-        },
+        method: "get",
+        path: "resolve",
       },
     },
   ],
 };
 
-export default config
+export default config;
